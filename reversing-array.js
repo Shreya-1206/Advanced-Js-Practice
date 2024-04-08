68c3aa1d61b4adcfd345a53755d337620115276c
Shreya <shreyaray.1206@gmail.com>
2024-04-08 21:38:10 +0530

    first commit

---
+++ reversing-array.js
@@ -0,0 +1,30 @@
+const tempRec = [
+    {
+        id : 1,
+        date : '5-March-2024',
+        maxTemp : 37,
+        minTemp : 28
+    },
+    {
+        id : 2,
+        date : '6-March-2024',
+        maxTemp : 36,
+        minTemp : 28
+    },
+    {
+        id : 3,
+        date : '7-March-2024',
+        maxTemp : 38,
+        minTemp : 28
+    },
+    {
+        id : 4,
+        date : '8-March-2024',
+        maxTemp : 35,
+        minTemp : 29
+    }
+];
+
+for(let x of tempRec.reverse()) {
+    console.log(`${x.date} : Max-Temp - ${x.maxTemp} || Min-Temp - ${x.minTemp}`);
+}
